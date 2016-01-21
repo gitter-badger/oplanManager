0d37813e5a37d6b1e76cb0362eb4211fb4907c6d
jZejda <zejda.jiri@gmail.com>
2016-01-21 22:34:22 +0100

    add menu model

---
+++ database/migrations/2016_01_21_210931_create_menus_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMenusTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('menus', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('label', 50);
+            $table->tyniInteger('dropdown');
+            $table->string('link', 100)->default('#');
+            $table->integer('parent')->default('0');
+            $table->integer('sort');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('menus');
+    }
+}
